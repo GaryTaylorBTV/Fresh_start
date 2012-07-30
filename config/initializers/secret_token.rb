b842f92595c099a8b3395683a6ea2132b5895fda
Gary Taylor <garytaylor11157@gmail.com>
2012-07-30 10:52:08 -0400

    Initial commit for Fresh_start.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e3a62f826904a6c142fe78797d294c7a2fe4e4cdc2060bb891ffb9472bf5541feef6a0c0538c2e4da98a185fc9caf1b3047279e438e69a7863b746270a251004'
